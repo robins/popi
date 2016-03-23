1c8bf6c7ff532b0ba1652b280b1ff3b1ede54df6
Robins Tharakan <tharakan@gmail.com>
2016-03-23 06:20:39 +0530

    Test wait should also be configurable via a single argument

--- runtests.sh
+++ runtests.sh
@@ -9,44 +9,45 @@ projVer=${proj}/$1/$t
 mkdir -p ${projVer}
 cd ${projVer}
 s=30
+w=100
 
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -T100 -U postgres pgbench 				&>c4j4T100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -S -T100 -U postgres pgbench 				&>c4j4ST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -T100 -U postgres pgbench 			&>c4j4MT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -S -T100 -U postgres pgbench 		&>c4j4MST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -f ../a.sql -T100 -U postgres pgbench 			&>c4j4FT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -f ../a.sql -S -T100 -U postgres pgbench 		&>c4j4FST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -f ../a.sql -T100 -U postgres pgbench 	&>c4j4MFT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -f ../a.sql -S -T100 -U postgres pgbench 	&>c4j4MFST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -T100 -U postgres pgbench 				&>c4j4CT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -S -T100 -U postgres pgbench 				&>c4j4CST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -M prepared -T100 -U postgres pgbench 		&>c4j4CMT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -M prepared -S -T100 -U postgres pgbench 		&>c4j4CMST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -T${w} -U postgres pgbench 				&>c4j4T100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -S -T${w} -U postgres pgbench 				&>c4j4ST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -T${w} -U postgres pgbench 			&>c4j4MT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -S -T${w} -U postgres pgbench 		&>c4j4MST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -f ../a.sql -T${w} -U postgres pgbench 			&>c4j4FT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -f ../a.sql -S -T${w} -U postgres pgbench 		&>c4j4FST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -f ../a.sql -T${w} -U postgres pgbench 	&>c4j4MFT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -M prepared -f ../a.sql -S -T${w} -U postgres pgbench 	&>c4j4MFST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -T${w} -U postgres pgbench 				&>c4j4CT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -S -T${w} -U postgres pgbench 				&>c4j4CST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -M prepared -T${w} -U postgres pgbench 		&>c4j4CMT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c4 -j4 -P1 -C -M prepared -S -T${w} -U postgres pgbench 		&>c4j4CMST100.txt
 
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -T100 -U postgres pgbench 				&>c8j4T100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -S -T100 -U postgres pgbench 				&>c8j4ST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -T100 -U postgres pgbench 			&>c8j4MT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -S -T100 -U postgres pgbench 		&>c8j4MST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -f ../a.sql -T100 -U postgres pgbench 			&>c8j4FT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -f ../a.sql -S -T100 -U postgres pgbench 		&>c8j4FST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -f ../a.sql -T100 -U postgres pgbench 	&>c8j4MFT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -f ../a.sql -S -T100 -U postgres pgbench 	&>c8j4MFST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -T100 -U postgres pgbench				&>c8j4CT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -S -T100 -U postgres pgbench 				&>c8j4CST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -M prepared -T100 -U postgres pgbench 		&>c8j4CMT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -M prepared -S -T100 -U postgres pgbench 		&>c8j4CMST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -T${w} -U postgres pgbench 				&>c8j4T100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -S -T${w} -U postgres pgbench 				&>c8j4ST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -T${w} -U postgres pgbench 			&>c8j4MT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -S -T${w} -U postgres pgbench 		&>c8j4MST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -f ../a.sql -T${w} -U postgres pgbench 			&>c8j4FT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -f ../a.sql -S -T${w} -U postgres pgbench 		&>c8j4FST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -f ../a.sql -T${w} -U postgres pgbench 	&>c8j4MFT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -M prepared -f ../a.sql -S -T${w} -U postgres pgbench 	&>c8j4MFST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -T${w} -U postgres pgbench				&>c8j4CT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -S -T${w} -U postgres pgbench 				&>c8j4CST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -M prepared -T${w} -U postgres pgbench 		&>c8j4CMT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c8 -j4 -P1 -C -M prepared -S -T${w} -U postgres pgbench 		&>c8j4CMST100.txt
 
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -T100 -U postgres pgbench 				&>c64j4T100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -S -T100 -U postgres pgbench 				&>c64j4ST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -T100 -U postgres pgbench 			&>c64j4MT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -S -T100 -U postgres pgbench 		&>c64j4MST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -f ../a.sql -T100 -U postgres pgbench 			&>c64j4FT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -f ../a.sql -S -T100 -U postgres pgbench 		&>c64j4FST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -f ../a.sql -T100 -U postgres pgbench 	&>c64j4MFT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -f ../a.sql -S -T100 -U postgres pgbench 	&>c64j4MFST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -T100 -U postgres pgbench 				&>c64j4CT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -S -T100 -U postgres pgbench 			&>c64j4CST100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -M prepared -T100 -U postgres pgbench 		&>c64j4CMT100.txt
-sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -M prepared -S -T100 -U postgres pgbench 		&>c64j4CMST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -T${w} -U postgres pgbench 				&>c64j4T100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -S -T${w} -U postgres pgbench 				&>c64j4ST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -T${w} -U postgres pgbench 			&>c64j4MT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -S -T${w} -U postgres pgbench 		&>c64j4MST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -f ../a.sql -T${w} -U postgres pgbench 			&>c64j4FT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -f ../a.sql -S -T${w} -U postgres pgbench 		&>c64j4FST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -f ../a.sql -T${w} -U postgres pgbench 	&>c64j4MFT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -M prepared -f ../a.sql -S -T${w} -U postgres pgbench 	&>c64j4MFST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -T${w} -U postgres pgbench 				&>c64j4CT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -S -T${w} -U postgres pgbench 			&>c64j4CST100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -M prepared -T${w} -U postgres pgbench 		&>c64j4CMT100.txt
+sleep $s; /opt/postgres/master/bin/pgbench -c64 -j4 -P1 -C -M prepared -S -T${w} -U postgres pgbench 		&>c64j4CMST100.txt
 
 /opt/postgres/master/bin/psql -U postgres -c 'SELECT version();' postgres 					> version.txt
