26a719da624cd2669b67e5ecc7f7fb8791d92110
Robins Tharakan <tharakan@gmail.com>
2016-03-24 04:53:06 +0000

    reload_pg now needs a folder name, since its now used at multiple places. TODO: commit reload_pg.sh soon

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ git pull
 make clean 
 make -j4
 sudo -u root -H sh -c "make install"
-sudo -u postgres -H sh -c "/bin/bash /home/postgres/reload_pg.sh"
+sudo -u postgres -H sh -c "/bin/bash /home/postgres/reload_pg.sh pgbench"
 sleep 10
 
 bash /home/robins/projects/pgbench/runtests.sh $2
