d6def28bbf6397a4ddd206a319f65efd436a2320
Robins Tharakan <tharakan@gmail.com>
2016-03-28 21:53:27 +0000

    runall now randomly generates the versions across which the benchmark is run against, and now crontab wise run every hour

--- runall.sh
+++ runall.sh
@@ -1,12 +1,24 @@
+# lock the script so only one runs at a time
+exec 200<$0
+flock -n 200 || exit 1
 
-# bash /home/robins/projects/run.sh REL9_1_STABLE 91
-# bash /home/robins/projects/run.sh REL9_2_STABLE 92
-# bash /home/robins/projects/run.sh REL9_3_STABLE 93
- bash /home/robins/projects/run.sh REL9_4_STABLE 94
- bash /home/robins/projects/run.sh REL9_5_STABLE 95
- bash /home/robins/projects/run.sh master master
+revisions="REL9_1_STABLE REL9_2_STABLE REL9_3_STABLE REL9_4_STABLE REL9_5_STABLE master"
+rev=$(echo ${revisions[@]} | tr " " "\n" | sort -R | tr "\n" " ")
 
-bash /home/robins/projects/run.sh REL9_1_STABLE 91
- bash /home/robins/projects/run.sh REL9_2_STABLE 92
- bash /home/robins/projects/run.sh REL9_3_STABLE 93
-#
+for s in $rev
+do
+	if [[ ${#s} -eq 6 ]]; then
+		s1=${s}
+		# Do nothing
+	else
+		s1=${s#REL}
+		s2=${s1%_STABLE}
+		s3=${s2/\_/\.}
+		s1=$s3
+	fi
+
+	bash /home/robins/projects/pgbench/run.sh $s $s1 9999
+
+	sleep 5s
+done
+
