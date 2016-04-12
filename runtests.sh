4269d8e5d5e8b1d72f52d1e828f9bc87daa98d35
Robins Tharakan <tharakan@gmail.com>
2016-04-13 02:12:07 +0530

    vertically align all tests

--- runtests.sh
+++ runtests.sh
@@ -35,44 +35,44 @@ echo "Runtest: Triggering battery of tests T=${t}" >> /home/robins/pgbench/log/h
 
 if [ $runtests -eq 1 ]; then
 
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -T${w} -U postgres pgbench         &>c4j4T100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -S -T${w} -U postgres pgbench         &>c4j4ST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -M prepared -T${w} -U postgres pgbench       &>c4j4MT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -M prepared -S -T${w} -U postgres pgbench     &>c4j4MST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -f ${q} -T${w} -U postgres pgbench       &>c4j4FT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -f ${q} -S -T${w} -U postgres pgbench     &>c4j4FST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -M prepared -f ${q} -T${w} -U postgres pgbench   &>c4j4MFT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -M prepared -f ${q} -S -T${w} -U postgres pgbench   &>c4j4MFST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -T${w} -U postgres pgbench         &>c4j4CT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -S -T${w} -U postgres pgbench       &>c4j4CST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -M prepared -T${w} -U postgres pgbench     &>c4j4CMT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -M prepared -S -T${w} -U postgres pgbench     &>c4j4CMST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}                           -T${w} -U postgres pgbench &>c4j4T100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}                        -S -T${w} -U postgres pgbench &>c4j4ST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}    -M prepared            -T${w} -U postgres pgbench &>c4j4MT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}    -M prepared         -S -T${w} -U postgres pgbench &>c4j4MST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -f ${q}                   -T${w} -U postgres pgbench &>c4j4FT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -f ${q}                -S -T${w} -U postgres pgbench &>c4j4FST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}    -M prepared -f ${q}    -T${w} -U postgres pgbench &>c4j4MFT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port}    -M prepared -f ${q} -S -T${w} -U postgres pgbench &>c4j4MFST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C                        -T${w} -U postgres pgbench &>c4j4CT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C                     -S -T${w} -U postgres pgbench &>c4j4CST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -M prepared            -T${w} -U postgres pgbench &>c4j4CMT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c4 -j4 -P1 -p ${port} -C -M prepared         -S -T${w} -U postgres pgbench &>c4j4CMST100.txt
 
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -T${w} -U postgres pgbench         &>c8j4T100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -S -T${w} -U postgres pgbench         &>c8j4ST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -M prepared -T${w} -U postgres pgbench       &>c8j4MT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -M prepared -S -T${w} -U postgres pgbench     &>c8j4MST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -f ${q} -T${w} -U postgres pgbench       &>c8j4FT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -f ${q} -S -T${w} -U postgres pgbench     &>c8j4FST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -M prepared -f ${q} -T${w} -U postgres pgbench   &>c8j4MFT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -M prepared -f ${q} -S -T${w} -U postgres pgbench   &>c8j4MFST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -T${w} -U postgres pgbench        &>c8j4CT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -S -T${w} -U postgres pgbench       &>c8j4CST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -M prepared -T${w} -U postgres pgbench     &>c8j4CMT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -M prepared -S -T${w} -U postgres pgbench     &>c8j4CMST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}                           -T${w} -U postgres pgbench &>c8j4T100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}                        -S -T${w} -U postgres pgbench &>c8j4ST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}    -M prepared            -T${w} -U postgres pgbench &>c8j4MT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}    -M prepared         -S -T${w} -U postgres pgbench &>c8j4MST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -f ${q}                   -T${w} -U postgres pgbench &>c8j4FT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -f ${q}                -S -T${w} -U postgres pgbench &>c8j4FST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}    -M prepared -f ${q}    -T${w} -U postgres pgbench &>c8j4MFT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port}    -M prepared -f ${q} -S -T${w} -U postgres pgbench &>c8j4MFST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C                        -T${w} -U postgres pgbench &>c8j4CT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C                     -S -T${w} -U postgres pgbench &>c8j4CST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -M prepared            -T${w} -U postgres pgbench &>c8j4CMT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c8 -j4 -P1 -p ${port} -C -M prepared         -S -T${w} -U postgres pgbench &>c8j4CMST100.txt
 
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -T${w} -U postgres pgbench         &>c64j4T100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -S -T${w} -U postgres pgbench         &>c64j4ST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -M prepared -T${w} -U postgres pgbench       &>c64j4MT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -M prepared -S -T${w} -U postgres pgbench     &>c64j4MST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -f ${q} -T${w} -U postgres pgbench       &>c64j4FT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -f ${q} -S -T${w} -U postgres pgbench     &>c64j4FST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -M prepared -f ${q} -T${w} -U postgres pgbench   &>c64j4MFT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -M prepared -f ${q} -S -T${w} -U postgres pgbench   &>c64j4MFST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -T${w} -U postgres pgbench         &>c64j4CT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -S -T${w} -U postgres pgbench       &>c64j4CST100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -M prepared -T${w} -U postgres pgbench     &>c64j4CMT100.txt
-  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -M prepared -S -T${w} -U postgres pgbench     &>c64j4CMST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}                           -T${w} -U postgres pgbench &>c64j4T100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}                        -S -T${w} -U postgres pgbench &>c64j4ST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}    -M prepared            -T${w} -U postgres pgbench &>c64j4MT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}    -M prepared         -S -T${w} -U postgres pgbench &>c64j4MST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -f ${q}                   -T${w} -U postgres pgbench &>c64j4FT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -f ${q}                -S -T${w} -U postgres pgbench &>c64j4FST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}    -M prepared -f ${q}    -T${w} -U postgres pgbench &>c64j4MFT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port}    -M prepared -f ${q} -S -T${w} -U postgres pgbench &>c64j4MFST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C                        -T${w} -U postgres pgbench &>c64j4CT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C                     -S -T${w} -U postgres pgbench &>c64j4CST100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -M prepared            -T${w} -U postgres pgbench &>c64j4CMT100.txt
+  sleep $s; ${bindir}/bin/pgbench -c64 -j4 -P1 -p ${port} -C -M prepared         -S -T${w} -U postgres pgbench &>c64j4CMST100.txt
 
 fi
 
