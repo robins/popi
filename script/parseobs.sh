b74960f1915783aa712b224e2e29a5388920fe6f
Robins Tharakan <tharakan@gmail.com>
2018-09-26 15:32:08 +0000

    Just convert the irritating spaces to tabs in parseobs

--- script/parseobs.sh
+++ script/parseobs.sh
@@ -7,16 +7,16 @@ obsdir=${basedir}/obs
 # Get all active versions from the internet
 # XXX: Ensure slow internet connections don't hold up this run
 versions=( `timeout -s SIGTERM 10 curl -so - "http://www.postgresql.org/support/versioning/" | \
-        grep -A100 "EOL" | \
-        grep -B2 "Yes" | \
-        grep "colFirst" | \
-        cut --bytes=25-27 | \
+		grep -A100 "EOL" | \
+		grep -B2 "Yes" | \
+		grep "colFirst" | \
+		cut --bytes=25-27 | \
 	sort | \
-        tr '\n' ' '` master)
+		tr '\n' ' '` master)
 
 if [ ${#versions[@]} -le 2 ]; then
 #        versions=(9.4 9.5 9.6 10 master)
-        versions=(master)
+		versions=(master)
 fi
 
 echo "Versions:  ${versions[@]}"
@@ -39,7 +39,7 @@ function GetTPSValue() {
 	f=/home/robins/projects/pgbench/obs/$1/$2/$3
 
 	if [ -f "$f" ]; then
- 		v=`grep "including connections" $f | awk '{print int($3)}'`
+		v=`grep "including connections" $f | awk '{print int($3)}'`
 	fi
 	if [[ "$v" =~ ^[0-9]+$ ]] && [ "$v" -ge 0 -a "$v" -le 1000000 ];
 	then
@@ -57,19 +57,19 @@ function GetAverageTPSValue() {
   n=0
   TPSTotal=0
   for t in {0..9};
-    do
-      tps=$(GetTPSValue $1 $t $2)
-      TPSTotal=`expr $TPSTotal + $tps`
-      if [[ $tps -gt 0 ]]; then
-        n=`expr $n + 1`
-      fi
-    done
+	do
+	  tps=$(GetTPSValue $1 $t $2)
+	  TPSTotal=`expr $TPSTotal + $tps`
+	  if [[ $tps -gt 0 ]]; then
+		n=`expr $n + 1`
+	  fi
+	done
 #echo $TPSTotal
 #echo $n
   if [ $n -eq 0 ]; then
-    echo 0
+	echo 0
   else
-    echo $(( TPSTotal / n ))
+	echo $(( TPSTotal / n ))
   fi
 #echo "done"
 }
@@ -78,41 +78,41 @@ function GetAverageTPSValue() {
 
 function iterateVer() {
 
-        filename=$1.txt
+		filename=$1.txt
 
-        t=$(getDescription $1)
-        echo $t
+		t=$(getDescription $1)
+		echo $t
 
-        for i in "${versions[@]}"
-        do
-                tps=$(GetAverageTPSValue $i $filename)
-                echo ${tps} 
-        done
+		for i in "${versions[@]}"
+		do
+				tps=$(GetAverageTPSValue $i $filename)
+				echo ${tps} 
+		done
 }
 
 function iterateCommit() {
 
-        output_filename=${basedir}/obs/results/$1.txt
+		output_filename=${basedir}/obs/results/$1.txt
 
-        git log --pretty=format:"%H" --after="2018-09-01" | tac | while read -r hash; do
-                f=${obsdir}/master/${test}.txt
+		git log --pretty=format:"%H" --after="2018-09-01" | tac | while read -r hash; do
+				f=${obsdir}/master/${test}.txt
 		if [ -f ${f} ]; then
 			tps=$(GetTPSValue $f)
- 	                echo ${tps} >> ${output_filename}
+					echo ${tps} >> ${output_filename}
 		fi
-        done
+		done
 }
 
 
 
 
 function iterateTest () {
-        find ${obsdir}/master/* -name ${1} | while read -r filepath; do
-                filename=${1}
-                echo ${filepath} ${filename}
-                #mkdir -p ${basedir}/obs/results/$line
-#               iterateVer ${filepath} ${filename}
-        done
+		find ${obsdir}/master/* -name ${1} | while read -r filepath; do
+				filename=${1}
+				echo ${filepath} ${filename}
+				iterateVer ${filepath} ${filename}
+exit 1
+		done
 }
 
 function iterateTests () {
